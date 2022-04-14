4dcb76af18217258fd48ca43292792cf555b5019
Susant Kumar Palai <palai.susant@gmail.com>
2022-04-14 15:57:05 -0700

    Extraction - done partially
    Signed-off-by: Susant Kumar Palai <palai.susant@gmail.com>

--- convertedShort.py
+++ convertedShort.py
@@ -1,8 +1,22 @@
+from http.client import ImproperConnectionState
+import queue
 import numpy as np
 import cv2
 import time
+import threading
+import os
 
 from threading import Thread
+from queue import Queue
+
+vidQueueLock = threading.Lock()
+
+videoQueue = Queue(maxsize=0)
+frameQueue = Queue(maxsize=0)
+
+videoPath = "./"
+framePath = videoPath + "frames/"
+os.makedirs(framePath, exist_ok=True)
 
 def captureVideo():
     cap = cv2.VideoCapture(0)
@@ -27,14 +41,41 @@ def captureVideo():
             # has time expired?
             if time.time() - timeStart >= duration:
                 count += 1
+                oldVidName = vidName
                 vidName = "vid-" + str(count) + ".avi"
                 timeStart = time.time()
                 out = cv2.VideoWriter(vidName, fourcc, frameRate, (w,h))
+    
+                #lock Video Queue
+                videoQueue.put(oldVidName)
             out.write(frame)
     cap.release()
     out.release()
     cv2.destroyAllWindows()
 
+def extractFrame(vidName):
+    os.system("ffmpeg -i " + str(videoPath+vidName) + " -r 1 " + str(framePath) + "image-"+"vidName"+".jpeg")
+    print("frame extracted")
+
+
+        
+def uploadVideo():
+    while True:
+        if (videoQueue.empty()):
+            time.sleep(1)
+        else:
+            # extract frames from video
+            extractFrame(videoQueue.get())
+
+
+def uploadFrame():
+    pass
+
+thread1 = Thread(target = uploadVideo, args = ())
+thread1.start()
+
 captureVideo()
+
+
 
 
